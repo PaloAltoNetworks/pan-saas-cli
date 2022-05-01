7acbec16058cb0f72ab49bd0366d9418e4f76330
Zack Macharia <blaqhrse@gmail.com>
2022-05-01 12:50:33 -0500

    fixed missing secret folder

--- SaaS-CLI/samplesecret/examplesecret.go
+++
@@ -1,6 +0,0 @@
-package secret
-
-// Secret: returns a 32 byte array used as the encryption and decryption key. This can be any 32byte array.
-func Secret() []byte {
-	return []byte("thisshouldbeany32bytearrayyoudig")
-}
