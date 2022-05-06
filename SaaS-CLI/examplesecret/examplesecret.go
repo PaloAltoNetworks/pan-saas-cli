636bb8cd9210a1d38f79073d64e7b6e236508701
Zack Macharia <blaqhrse@gmail.com>
2022-05-06 07:50:26 -0500

    added comment to Secret func in examplesecret pkg

--- SaaS-CLI/examplesecret/examplesecret.go
+++ SaaS-CLI/examplesecret/examplesecret.go
@@ -2,5 +2,5 @@ package examplesecret
 
 // Secret: returns a 32 byte array used as the encryption and decryption key. This can be any 32byte array.
 func Secret() []byte {
-	return []byte("thisshouldbeany32bytearrayyoudig")
+	return []byte("thisshouldbeany32bytearrayyoudig") // CHANGE THIS VALUE TO YOUR OWN VALUE
 }
